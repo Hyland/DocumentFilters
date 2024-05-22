f6dd1881ddf07b94e3965eaeffd8aef54bb0b017
Brian Grady <Brian.Grady@hyland.com>
2024-05-22 11:10:05 -0700

    Update version strings to 24.2.1

--- samples/python/DocumentFiltersSample.py
+++ samples/python/DocumentFiltersSample.py
@@ -1,7 +1,7 @@
 import os, platform, zipfile, urllib.request, fnmatch, DocumentFiltersLicense
 from DocumentFilters import *
 
-DF_VERSION='24.1'
+DF_VERSION='24.2.1'
 with open('../DF_VERSION.txt', 'r') as version_file:
     DF_VERSION=version_file.read().strip()
 
